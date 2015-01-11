c86489c0ede699fc3d30ec540bd42ca0664165ee
Andreas Helmberger <public@ahelmberger.de>
2015-01-11 23:45:15 +0100

    Order files by version read from pe header

--- file_tools.py
+++ file_tools.py
@@ -1,5 +1,5 @@
 import os
-import time
+import win32api
 
 
 def get_file_comparison_table(directories):
@@ -9,14 +9,25 @@ def get_file_comparison_table(directories):
     return file_table
 
 
+def get_version(file_path):
+    try:
+        info = win32api.GetFileVersionInfo(file_path, "\\")
+        ms = info["FileVersionMS"]
+        ls = info["FileVersionLS"]
+        version = "%d.%d.%d.%d" % (win32api.HIWORD(ms), win32api.LOWORD(ms), win32api.HIWORD(ls), win32api.LOWORD(ls))
+    except:
+        version = "0.0.0.0"
+    return version
+
+
 def fill_file_info_dictionary(dictionary, path):
     filelist = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     for file in filelist:
         filename = os.path.join(path, file)
-        props = os.stat(filename)
         if file not in dictionary:
             dictionary[file] = []
-        dictionary[file] += [(path, time.ctime(props.st_ctime))]
+        info_tuple = (path, get_version(filename))
+        dictionary[file] = sorted(dictionary[file] + [info_tuple], key=lambda item: item[1])
 
 
 path1 = os.path.join(os.environ["HOMEDRIVE"], os.environ["HOMEPATH"], "Desktop\\VSTPlugins 64 bit")
