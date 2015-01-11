4025cc4c228f57630fcaaf6af3a3dfffa7a0a99d
Andreas Helmberger <public@ahelmberger.de>
2015-01-11 22:23:16 +0100

    Add function for aggregating a table for multiple directories

--- file_tools.py
+++ file_tools.py
@@ -1,21 +1,29 @@
 import os
+import time
+
+
+def get_file_comparison_table(directories):
+    file_table = {}
+    for directory in directories:
+        fill_file_info_dictionary(file_table, directory)
+    return file_table
 
 
 def fill_file_info_dictionary(dictionary, path):
-    filelist = os.listdir(path)
+    filelist = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     for file in filelist:
         filename = os.path.join(path, file)
         props = os.stat(filename)
         if file not in dictionary:
             dictionary[file] = []
-        dictionary[file] += [(filename, props)]
+        dictionary[file] += [(path, time.ctime(props.st_ctime))]
 
 
-
-dict = {}
-dllPath = os.path.join(os.environ["HOMEDRIVE"], os.environ["HOMEPATH"], "Desktop\\VSTPlugins 64 bit")
-fill_file_info_dictionary(dict, dllPath)
-print(dict)
+path1 = os.path.join(os.environ["HOMEDRIVE"], os.environ["HOMEPATH"], "Desktop\\VSTPlugins 64 bit")
+path2 = os.path.join(os.environ["HOMEDRIVE"], os.environ["HOMEPATH"], "Desktop\\VSTPlugins")
+paths = [path1, path2]
+table = get_file_comparison_table(paths)
+print(table)
 
 
 
