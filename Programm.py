baecf7e1aeb3e76a2f58685696c1856a959a8a92
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2015-01-11 20:33:02 +0100

    Initial commit

---
+++ Programm.py
@@ -0,0 +1,12 @@
+__author__ = 'Ulrich Kaiser'
+
+import Comparer
+
+dllPath = "C:\\Users\\Ulrich\Desktop\\Dll-Tool\\VSTPlugins 64 bit"
+
+
+def main():
+    print(Comparer.writeinfos(dllPath))
+
+if __name__ == "__main__":
+    main()
